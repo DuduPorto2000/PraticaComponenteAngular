c41234022796a66d408486f67764f0bef96b5184
DuduPorto2000 <dudu@porto.ws>
2021-08-22 21:55:54 -0300

    1 file modified

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button/button-module';
-import { MatMenuModule } from '@angular/material/menu/menu-module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+
 
 
 
